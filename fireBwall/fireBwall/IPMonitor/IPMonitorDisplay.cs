7640732ae27f04b5016cd9076a61db989439935a
bryan alexander <shodivine@gmail.com>
2012-06-07 01:25:49 -0500

    * Converted IPMonitor to new architecture

---
+++ fireBwall/fireBwall/IPMonitor/IPMonitorDisplay.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.NetworkInformation;
+using System.Net;
+
+using fireBwall.Packets;
+using fireBwall.UI;
+using fireBwall.Utils;
+
+namespace IPMonitor
+{
+    public partial class IPMonitorDisplay : DynamicUserControl
+    {
+        // local ipmonitoring mod
+        private IPMonitorModule ipmon;
+        
+        // local tcp cache
+        private List<TcpConnectionInformation> tcpcache;
+        public List<TcpConnectionInformation> Tcpcache
+             { get { return tcpcache; } set { tcpcache = new List<TcpConnectionInformation>(value); } }
+
+        // local udp cache
+        private List<IPEndPoint> udpcache;
+        public List<IPEndPoint> Udpcache
+            { get { return udpcache; } set { udpcache = new List<IPEndPoint>(value); } }
+
+        public IPMonitorDisplay(IPMonitorModule mon)
+        {
+            this.ipmon = mon;
+            tcpcache = new List<TcpConnectionInformation>();
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Update the TCP connections
+        /// </summary>
+        private void TCPUpdate()
+        {
+            // preserve the idx of the viewport
+            int idx = tcpDisplay.FirstDisplayedScrollingRowIndex;
+
+            // if the datasource hasn't been set, set it
+            if (tcpDisplay.DataSource != this.tcpcache)
+                tcpDisplay.DataSource = this.tcpcache;
+
+            // update local tcpcache
+            tcpcache = new List<TcpConnectionInformation>(ipmon.Tcpcache);
+            
+            // -1 = no viewport
+            if ( idx >= 0 )
+                tcpDisplay.FirstDisplayedScrollingRowIndex = idx;
+
+            // set the total connections label
+            tcpTotal.Text = String.Format("{0}: {1}","Connections", tcpcache.Count.ToString());
+        }
+
+        /// <summary>
+        /// Update the UDP connections
+        /// </summary>
+        private void UDPUpdate()
+        {
+            int idx = udpDisplay.FirstDisplayedScrollingRowIndex;
+
+            if (udpDisplay.DataSource != this.udpcache)
+                udpDisplay.DataSource = this.udpcache;
+
+            udpcache = new List<IPEndPoint>(ipmon.Udpcache);
+
+            if (idx >= 0)
+                udpDisplay.FirstDisplayedScrollingRowIndex = idx;
+
+            udpTotal.Text = String.Format("{0}: {1}", "Listeners", udpcache.Count.ToString());
+        }
+
+        /// <summary>
+        /// Set the TCP statistics fields
+        /// </summary>
+        private void DumpStats()
+        {
+            IPGlobalProperties stats = IPGlobalProperties.GetIPGlobalProperties();
+            TcpStatistics tcpstat = stats.GetTcpIPv4Statistics();
+            UdpStatistics udpstat = stats.GetUdpIPv4Statistics();
+
+            // set labels 
+            // this doesn't need to be done each time
+            setLabels();
+
+            // set the TCP field labels
+            connAcceptField.Text = tcpstat.ConnectionsAccepted.ToString();
+            connInitiatedField.Text = tcpstat.ConnectionsInitiated.ToString();
+            cumulativeConnectionsField.Text = tcpstat.CumulativeConnections.ToString();
+
+            errorsReceivedField.Text = tcpstat.ErrorsReceived.ToString();
+            failedConAttField.Text = tcpstat.FailedConnectionAttempts.ToString();
+            maxConnField.Text = tcpstat.MaximumConnections.ToString();
+
+            resetConnField.Text = tcpstat.ResetConnections.ToString();
+            resetsSentField.Text = tcpstat.ResetsSent.ToString();
+            segsReceivedField.Text = tcpstat.SegmentsReceived.ToString();
+
+            segsResentField.Text = tcpstat.SegmentsResent.ToString();
+            segsSentField.Text = tcpstat.SegmentsSent.ToString();
+
+            // set the UDP field labels
+            dataReceivedField.Text = udpstat.DatagramsReceived.ToString();
+            dataSentField.Text = udpstat.DatagramsSent.ToString();
+
+            incDataDiscField.Text = udpstat.IncomingDatagramsDiscarded.ToString();
+            incDataErrField.Text = udpstat.IncomingDatagramsWithErrors.ToString();
+        }
+        /// <summary>
+        /// invoke the TCP datagridview updater
+        /// </summary>
+        public void UpdateTCP()
+        {
+            if (tcpDisplay.InvokeRequired)
+            {
+                System.Threading.ThreadStart up = new System.Threading.ThreadStart(TCPUpdate);
+                tcpDisplay.Invoke(up);
+            }
+            else
+                TCPUpdate();
+        }
+
+        /// <summary>
+        /// invoke the UDP datagridview updater
+        /// </summary>
+        public void UpdateUDP()
+        {
+            if (udpDisplay.InvokeRequired)
+            {
+                System.Threading.ThreadStart up = new System.Threading.ThreadStart(UDPUpdate);
+                udpDisplay.Invoke(up);
+            }
+            else
+                UDPUpdate();
+        }
+
+        /// <summary>
+        /// invoke the statistics updater
+        /// </summary>
+        public void UpdateStats()
+        {
+            if (statistics.InvokeRequired)
+            {
+                System.Threading.ThreadStart up = new System.Threading.ThreadStart(DumpStats);
+                statistics.Invoke(up);
+            }
+            else
+                DumpStats();
+        }
+
+        /// <summary>
+        /// set the labels as language-specific
+        /// </summary>
+        private void setLabels()
+        {
+            connAcceptLabel.Text = ipmon.multistring.GetString("Connections Accepted");
+            connInitLabel.Text = ipmon.multistring.GetString("Connections Initiated");
+            cumulativeConnLabel.Text = ipmon.multistring.GetString("Cumulative Connections");
+            errRecLabel.Text = ipmon.multistring.GetString("Errors Received");
+            failedConnAttLabel.Text = ipmon.multistring.GetString("Failed Connection Attempts");
+            maxConLabel.Text = ipmon.multistring.GetString("Maximum Connections");
+            resetConLabel.Text = ipmon.multistring.GetString("Reset Connections");
+            resetsSentLabel.Text = ipmon.multistring.GetString("Resets Sent");
+            segRecLabel.Text = ipmon.multistring.GetString("Segments Received");
+            segResentLabel.Text = ipmon.multistring.GetString("Segments Resent");
+            segSentLabel.Text = ipmon.multistring.GetString("Segments Sent");
+            dataRecLabel.Text = ipmon.multistring.GetString("Datagrams Received");
+            dataSentLabel.Text = ipmon.multistring.GetString("Datagrams Sent");
+            incDataDiscLabel.Text = ipmon.multistring.GetString("Incoming Datagrams Discarded");
+            incDataWErrLabel.Text = ipmon.multistring.GetString("Incoming Datagrams With Errors");
+        }
+    }
+}
---
+++ fireBwall/fireBwall/IPMonitor/fireBwallModule.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+using System.Timers;
+
+using fireBwall.Utils;
+using fireBwall.Configuration;
+using fireBwall.Modules;
+using fireBwall.Logging;
+using fireBwall.Packets;
+
+namespace IPMonitor
+{
+    /// <summary>
+    /// IP monitoring module for viewing listening connections, i/o, and 
+    /// traffic
+    /// </summary>
+    public class IPMonitorModule : NDISModule
+    {
+        // cache of TCP connections
+        private List<TcpConnectionInformation> tcpcache = new List<TcpConnectionInformation>();
+        public List<TcpConnectionInformation> Tcpcache 
+                { get { return tcpcache; } set { tcpcache = new List<TcpConnectionInformation>(value); } }
+
+        // cache of UDP listeners
+        private List<IPEndPoint> udpcache = new List<IPEndPoint>();
+        public List<IPEndPoint> Udpcache
+            { get { return udpcache; } set { udpcache = new List<IPEndPoint>(value); } }
+
+        // ipdisplay
+        private IPMonitorDisplay ipmon;
+
+        // update timer
+        private System.Timers.Timer updateTimer = new System.Timers.Timer();
+
+        public MultilingualStringManager multistring = new MultilingualStringManager();
+
+         /// <summary>
+         /// Class constructor
+         /// </summary>
+        public IPMonitorModule()
+            : base()
+        {
+            Help();
+        }
+
+        /// <summary>
+        /// Returns a GUI handle
+        /// </summary>
+        /// <returns></returns>
+        public override fireBwall.UI.DynamicUserControl GetUserInterface()
+        {
+            try
+            {
+                ipmon = new IPMonitorDisplay(this) { Dock = System.Windows.Forms.DockStyle.Fill };
+                // force a tick when the GUI is loaded
+                timer_Tick(updateTimer, null);
+            }
+            catch (Exception ex)
+            {
+                LogCenter.Instance.LogException(ex);
+            }
+            return ipmon;
+        }
+
+        /// <summary>
+        /// Starts the module
+        /// </summary>
+        /// <returns></returns>
+        public override bool ModuleStart()
+        {
+            try
+            {
+                ipmon = new IPMonitorDisplay(this) { Dock = System.Windows.Forms.DockStyle.Fill };
+
+                // configure the update timer to refresh the caches every
+                // 5 seconds
+                updateTimer.Elapsed += new ElapsedEventHandler(timer_Tick);
+                updateTimer.Interval = 5000;
+                updateTimer.Enabled = true;
+                updateTimer.Start();
+            }
+            catch (Exception e)
+            {
+                LogCenter.Instance.LogException(e);
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// ModuleStop
+        /// </summary>
+        /// <returns></returns>
+        public override bool ModuleStop()
+        {
+            // why not
+            return true;
+        }
+
+        /// <summary>
+        /// main module routine.
+        /// Right now it doesn't do anything with individual packets, but in the future the
+        /// user will be able to parse through data based on a certain connection.
+        /// </summary>
+        /// <param name="in_packet"></param>
+        /// <returns></returns>
+        public override PacketMainReturnType interiorMain(ref Packet in_packet)
+        {
+            return PacketMainReturnType.Allow;
+        }
+
+        /// <summary>
+        /// Event handler for when the 5 second update timer goes off.
+        /// Updates both the local TCP/UDP caches, then calls the GUI invoker methods.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            UpdateTCP();
+            UpdateUDP();
+            ipmon.UpdateTCP();
+            ipmon.UpdateUDP();
+            ipmon.UpdateStats();
+        }
+
+        /// <summary>
+        /// Method used to update TCP connections
+        /// </summary>
+        private void UpdateTCP()
+        {
+            // get the connection info
+            IPGlobalProperties ipGlob = IPGlobalProperties.GetIPGlobalProperties();
+            List<TcpConnectionInformation> tcpInfo = new List<TcpConnectionInformation>(ipGlob.GetActiveTcpConnections());
+            List<TcpConnectionInformation> temp = new List<TcpConnectionInformation>(tcpcache);
+
+            // remove invalid connections
+            foreach (TcpConnectionInformation tci in temp)
+            {
+                if (!(tcpInfo.Contains(tci)))
+                    tcpcache.Remove(tci);
+            }
+            
+            // if the cache doesn't contain the TcpConnection, add it
+            foreach (TcpConnectionInformation tci in tcpInfo)
+            {
+                if ( !(tcpcache.Contains(tci)))
+                    tcpcache.Add(tci);
+            }
+        }
+
+        /// <summary>
+        /// Method used to update UDP connections
+        /// </summary>
+        private void UpdateUDP()
+        {
+            // get the connection info
+            IPGlobalProperties ipGlob = IPGlobalProperties.GetIPGlobalProperties();
+            List<IPEndPoint> udpInfo = new List<IPEndPoint>(ipGlob.GetActiveUdpListeners());
+            List<IPEndPoint> temp = new List<IPEndPoint>(udpcache);
+
+            // remove old connections
+            foreach (IPEndPoint ip in temp)
+            {
+                if (!(udpInfo.Contains(ip)))
+                    udpcache.Remove(ip);
+            }
+            
+            // add new connections
+            foreach (IPEndPoint ip in udpInfo)
+            {
+                if ( !(udpcache.Contains(ip)))
+                    udpcache.Add(ip);
+            }
+        }
+
+        // module metadata
+        private void Help()
+        {
+            ModuleMeta.Meta m = new ModuleMeta.Meta();
+            m.Name = "IP Monitor";
+            m.Version = "1.0.2.0";
+            m.Description = "Displays varying information about network connections";
+            m.Contact = "shodivine@gmail.com";
+            m.Author = "Bryan A. (drone)";
+            m.Help = "The IP Monitor module displays connections and their corresponding ports on the localhost's TCP/UDP sockets.  The list is "
+                + "refreshed every 5 seconds to keep up with the frequency of lost/established connections.  The 'state' column of TCP connections can be one of "
+                + "nine different states.  They are: \n"
+                + "1. LISTENING\t\tIn case of a server, waiting for a connection request form any remote client.\n"
+                + "2. SYN-SENT\t\tWaiting for a remote peer to send back a SYN/ACK.\n"
+                + "3. SYN-RECEIVED\t\tWaiting for a remote peer to send back an ACK.\n"
+                + "4. ESTABLISHED\t\tThe port is ready to receive/send data to/from a peer.\n"
+                + "5/6. FIN-WAIT-(1|2)\t\tThe client is waiting for the server's FIN.\n"
+                + "7.. CLOSE-WAIT\t\tIndicates the server is sending it's own FIN.\n"
+                + "8. TIME-WAIT\t\tWaiting to ensure peer received teardown ACK.\n"
+                + "9. CLOSED\t\tConnection is closed.\n\n"
+                + "The format of connections is IP:PORT";
+
+            MetaData = new ModuleMeta(m);
+
+            Language lang = Language.ENGLISH;
+            multistring.SetString(lang, "Connections Accepted", "Connections Accepted:");
+            multistring.SetString(lang, "Connections Initiated", "Connections Initiated:");
+            multistring.SetString(lang, "Cumulative Connections", "Cumulative Connections:");
+            multistring.SetString(lang, "Errors Received", "Errors Received:");
+            multistring.SetString(lang, "Failed Connection Attempts", "Failed Connection Attempts:");
+            multistring.SetString(lang, "Maximum Connections", "Maximum Connections:");
+            multistring.SetString(lang, "Reset Connections", "Reset Connections:");
+            multistring.SetString(lang, "Resets Sent", "Resets Sent:");
+            multistring.SetString(lang, "Segments Received", "Segments Received:");
+            multistring.SetString(lang, "Segments Resent", "Segments Resent:");
+            multistring.SetString(lang, "Segments Sent", "Segments Sent:");
+            multistring.SetString(lang, "Datagrams Received", "Datagrams Received:");
+            multistring.SetString(lang, "Datagrams Sent", "Datagrams Sent:");
+            multistring.SetString(lang, "Incoming Datagrams Discarded", "Incoming Datagrams Discarded:");
+            multistring.SetString(lang, "Incoming Datagrams With Errors", "Incoming Datagrams With Errors:");
+            
+        }
+    }
+}
